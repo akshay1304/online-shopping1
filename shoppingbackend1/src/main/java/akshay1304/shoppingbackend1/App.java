3c64cfa33612680bd41b290d1b60d08342b40dcb
Akshay <akshay1304@gmail.com>
2018-04-11 17:55:38 +0530

    My project setup completed successfully

---
+++ shoppingbackend1/src/main/java/akshay1304/shoppingbackend1/App.java
@@ -0,0 +1,13 @@
+package akshay1304.shoppingbackend1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
